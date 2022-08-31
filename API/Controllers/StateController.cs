318eeba268b018cd78de54320a371955b06bb80e
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-08-31 23:53:48 +0100

    Add swagger XML Comments

--- API/Controllers/StateController.cs
+++ API/Controllers/StateController.cs
@@ -13,27 +13,55 @@ namespace API.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Returns top level states
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<List<State>>> GetStates() =>
             Ok(await _mediator.Send(new List.Query()));
 
-
+        /// <summary>
+        /// Returns list of states for the given parent state
+        /// </summary>
+        /// <param name="parentStateID"></param>
+        /// <returns></returns>
         [HttpGet("parent/{parentStateID}")]
         public async Task<ActionResult<List<State>>> GetStatesForParentStateId(int parentStateID) =>
             Ok(await _mediator.Send(new DetailsParent.Query() { ParentStateID = parentStateID }));
 
+        /// <summary>
+        /// Returns the requested state
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<State>> GetStateById(int id) =>
             Ok(await _mediator.Send(new Details.Query() { StateId = id }));
 
+        /// <summary>
+        /// Creates a new state
+        /// </summary>
+        /// <param name="state">State object to be created</param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> Create(State state)
         => Created(string.Empty, await _mediator.Send(new Create.Command() { State = state }));
 
+        /// <summary>
+        /// Deletes state based on state id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<ActionResult<State>> DeleteStateById(int id) =>
             Ok(await _mediator.Send(new Delete.Command() { Id = id }));
 
+        /// <summary>
+        /// Updates / Replaces a state
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
         [HttpPut]
         public async Task<ActionResult<State>> Edit(State state) =>
             Ok(await _mediator.Send(new Edit.Command() { State = state }));
--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using Storage;
 using MediatR;
 using Application.States;
 using Application.Core;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +21,11 @@ builder.Services.AddControllers().AddJsonOptions(jsonOptions =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 builder.Services.AddMediatR(typeof(List.Query).Assembly);
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
