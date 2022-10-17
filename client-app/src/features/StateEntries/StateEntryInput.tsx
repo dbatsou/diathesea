1863ea6ba257a9afaaee4eeeac20d6b4c3efaef1
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-10-17 02:33:50 +0100

    Add StateEntriesDashboard

---
+++ client-app/src/features/StateEntries/StateEntryInput.tsx
@@ -0,0 +1,23 @@
+import { Button, Container, Form, TextArea } from "semantic-ui-react";
+import { StateFormFormatted } from "../../app/models/state";
+
+interface Props {
+  states: StateFormFormatted[];
+}
+
+const StateEntryInput = ({ states }: Props) => (
+  <Container>
+    <Form>
+      <Form.Select options={states} placeholder="State" />
+      <TextArea placeholder="Tell us more" />
+      <Button
+        floated="right"
+        content="Save"
+        color="green"
+        style={{ marginTop: "1em" }}
+      />
+    </Form>
+  </Container>
+);
+
+export default StateEntryInput;
