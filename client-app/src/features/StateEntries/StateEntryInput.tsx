3aa659a87cc65e9b305e8301b9cd3ea4a089c1eb
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-10-17 02:49:03 +0100

    Add datepicker

--- client-app/src/features/StateEntries/StateEntryInput.tsx
+++ client-app/src/features/StateEntries/StateEntryInput.tsx
@@ -1,3 +1,4 @@
+import SemanticDatepicker from "react-semantic-ui-datepickers";
 import { Button, Container, Form, TextArea } from "semantic-ui-react";
 import { StateFormFormatted } from "../../app/models/state";
 
@@ -8,6 +9,8 @@ interface Props {
 const StateEntryInput = ({ states }: Props) => (
   <Container>
     <Form>
+      <SemanticDatepicker />
+
       <Form.Select options={states} placeholder="State" />
       <TextArea placeholder="Tell us more" />
       <Button
