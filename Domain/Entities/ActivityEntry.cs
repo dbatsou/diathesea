5655e3e464bfb506366328e3383ec1849235ff8c
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-10-17 01:29:14 +0100

    ActivityEntry: Remove Note property

--- Domain/Entities/ActivityEntry.cs
+++ Domain/Entities/ActivityEntry.cs
@@ -8,7 +8,6 @@ namespace Domain.Entities
         public int ActivityEntryId { get; set; }
         public int ActivityId { get; set; }
         public Activity Activity { get; set; }
-        public string Note { get; set; }
         public State? State { get; set; }
         public int? StateId { get; set; }
     }
