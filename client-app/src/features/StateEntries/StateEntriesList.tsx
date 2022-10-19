16fe132dc8815a453e96f1d36310ccd8b720e3bb
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-10-19 04:51:58 +0100

    Add StateEntriesDashboard and it's components

--- client-app/src/features/StateEntries/StateEntriesList.tsx
+++ client-app/src/features/StateEntries/StateEntriesList.tsx
@@ -1,13 +1,20 @@
-import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { Button, Divider, Item, Label, Segment } from "semantic-ui-react";
 import { StateEntry } from "../../app/models/stateEntry";
 
 interface Props {
   stateEntries: StateEntry[];
+  selectStateEntry: (id: number) => void;
+  deleteStateEntry: (id: number) => void;
 }
 
-export default function StateEntriesList({ stateEntries }: Props) {
+export default function StateEntriesList({
+  stateEntries,
+  selectStateEntry,
+  deleteStateEntry,
+}: Props) {
   return (
     <Segment>
+      {/* <Divider /> */}
       <Item.Group divided>
         {stateEntries.map((stateEntry) => (
           <Item key={stateEntry.StateEntryId}>
@@ -19,15 +26,18 @@ export default function StateEntriesList({ stateEntries }: Props) {
               <Item.Description>
                 <div>{stateEntry.Note}</div>
               </Item.Description>
-              <Item.Extra>
-                <Button
-                  //onClick={() => deleteActivity(activityentry.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                />
-                {/* <Label basic content={activityentry.category} />  */}
-              </Item.Extra>
+              <Button
+                onClick={() => deleteStateEntry(stateEntry.StateEntryId)}
+                floated="right"
+                content="Delete"
+                color="red"
+              />
+              <Button
+                onClick={() => selectStateEntry(stateEntry.StateEntryId)}
+                floated="right"
+                content="Edit"
+                color="blue"
+              />
             </Item.Content>
           </Item>
         ))}
--- client-app/src/features/StateEntries/StateEntryInput.tsx
+++ client-app/src/features/StateEntries/StateEntryInput.tsx
@@ -1,26 +1,51 @@
 import SemanticDatepicker from "react-semantic-ui-datepickers";
-import { Button, Container, Form, TextArea } from "semantic-ui-react";
+import { Button, Form, Segment, TextArea } from "semantic-ui-react";
 import { StateFormFormatted } from "../../app/models/state";
+import { StateEntry } from "../../app/models/stateEntry";
 
 interface Props {
   states: StateFormFormatted[];
+  selectedStateEntry: StateEntry | undefined;
+  cancel: () => void;
 }
 
-const StateEntryInput = ({ states }: Props) => (
-  <Container>
+const StateEntryInput = ({ states, selectedStateEntry, cancel }: Props) => (
+  <Segment>
     <Form>
-      <SemanticDatepicker />
-
-      <Form.Select options={states} placeholder="State" />
-      <TextArea placeholder="Tell us more" />
+      <SemanticDatepicker
+        format="dddd DD MMMM YYYY"
+        id="datepicker"
+        value={selectedStateEntry && new Date(selectedStateEntry?.CreatedAt)}
+        placeholder="Date"
+      />
+      <Form.Select
+        options={states}
+        placeholder="State"
+        value={
+          selectedStateEntry
+            ? states.find((x) => x.stateid === selectedStateEntry.StateId)
+                ?.value
+            : ""
+        }
+      />
+      <TextArea
+        placeholder="Tell us more"
+        value={selectedStateEntry ? selectedStateEntry?.Note : ""}
+      />
       <Button
         floated="right"
-        content="Save"
+        content={selectedStateEntry ? "Update" : "Save"}
         color="green"
-        style={{ marginTop: "1em" }}
+        style={{ marginTop: "2em" }}
+      />
+      <Button
+        floated="right"
+        onClick={() => cancel()}
+        content="Cancel"
+        style={{ marginTop: "2em" }}
       />
     </Form>
-  </Container>
+  </Segment>
 );
 
 export default StateEntryInput;
