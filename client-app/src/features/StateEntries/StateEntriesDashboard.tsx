f1536e10b446f52b156108b4e49b8a8a4474c43c
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-10-19 04:51:58 +0100

    Add StateEntriesDashboard and it's components

--- client-app/src/features/StateEntries/StateEntriesDashboard.tsx
+++ client-app/src/features/StateEntries/StateEntriesDashboard.tsx
@@ -1,4 +1,4 @@
-import { Container } from "semantic-ui-react";
+import { Button, Container } from "semantic-ui-react";
 import { StateFormFormatted } from "../../app/models/state";
 import { StateEntry } from "../../app/models/stateEntry";
 import StateEntriesList from "./StateEntriesList";
@@ -7,13 +7,38 @@ import StateEntryInput from "./StateEntryInput";
 interface Props {
   states: StateFormFormatted[];
   stateEntries: StateEntry[];
+  selectedStateEntry: StateEntry | undefined;
+  selectStateEntry: (id: number) => void;
+  cancelStateEntry: () => void;
+  editMode: boolean;
+  openForm: (id: number) => void;
+  deleteStateEntry: (id: number) => void;
+  closeForm: () => void;
 }
 
-export default function StateEntriesDashboard({ states, stateEntries }: Props) {
+export default function StateEntriesDashboard({
+  states,
+  stateEntries,
+  selectedStateEntry,
+  deleteStateEntry,
+  selectStateEntry,
+  cancelStateEntry,
+  editMode,
+  openForm,
+  closeForm,
+}: Props) {
   return (
     <Container>
-      <StateEntriesList stateEntries={stateEntries} />
-      <StateEntryInput states={states} />
+      <StateEntriesList
+        stateEntries={stateEntries}
+        selectStateEntry={selectStateEntry}
+        deleteStateEntry={deleteStateEntry}
+      />
+      <StateEntryInput
+        states={states}
+        selectedStateEntry={selectedStateEntry}
+        cancel={cancelStateEntry}
+      />
     </Container>
   );
 }
