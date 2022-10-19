62dd03a347b8cee09b5e7ba5a4ed99e1d8d28d1c
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-10-19 04:52:14 +0100

    Updates to <App>

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,14 +2,59 @@ import { useEffect, useState } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import agent from "../api/agent";
-import { State, StateFormFormatted } from "../models/state";
+import { StateFormFormatted } from "../models/state";
 import { StateEntry } from "../models/stateEntry";
 import StateEntriesDashboard from "../../features/StateEntries/StateEntriesDashboard";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
   // const [states, SetStates] = useState<State[]>([]);
   const [stateEntries, SetStateEntries] = useState<StateEntry[]>([]);
   const [states, SetStates] = useState<StateFormFormatted[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [selectedStateEntry, setSelectedStateEntry] = useState<
+    StateEntry | undefined
+  >(undefined);
+
+  function handleSelectStateEntry(id: number) {
+    var entry = stateEntries.find((x) => x.StateEntryId === id);
+    setSelectedStateEntry(entry);
+    setEditMode(true);
+    console.log("set to " + id);
+    console.log(entry);
+  }
+
+  function handleCancelSelectStateEntry() {
+    setSelectedStateEntry(undefined);
+    setEditMode(false);
+    console.log("cancel", selectedStateEntry);
+  }
+
+  function handleFormOpen(id?: number) {
+    id ? handleSelectStateEntry(id) : handleCancelSelectStateEntry();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleDeleteStateEntry(id: number) {
+    setLoading(true);
+
+    agent.StateEntries.delete(id).then();
+    {
+      var filteredStateEntries = stateEntries.filter(
+        (x) => x.StateEntryId !== id
+      );
+      var removed = stateEntries.find((x) => x.StateEntryId === id);
+      SetStateEntries(filteredStateEntries);
+      console.log("removed: ", removed);
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
     agent.States.list().then((response) => {
@@ -19,10 +64,11 @@ function App() {
           key: value.StateId,
           text: value.StateName,
           value: value.StateName,
+          stateid: value.StateId,
         });
+        SetStates(statesFormatted);
+        setLoading(false);
       });
-      console.log(statesFormatted);
-      SetStates(statesFormatted);
     });
 
     agent.StateEntries.list().then((response) => {
@@ -30,11 +76,22 @@ function App() {
     });
   }, []);
 
+  if (isLoading) return <LoadingComponent />;
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: "4em" }}>
-        <StateEntriesDashboard stateEntries={stateEntries} states={states} />
+        <StateEntriesDashboard
+          stateEntries={stateEntries}
+          states={states}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          selectStateEntry={handleSelectStateEntry}
+          editMode={editMode}
+          selectedStateEntry={selectedStateEntry}
+          cancelStateEntry={handleCancelSelectStateEntry}
+          deleteStateEntry={handleDeleteStateEntry}
+        />
       </Container>
     </>
   );
