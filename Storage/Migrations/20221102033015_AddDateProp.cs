c80b0934ef0b9be81e78558f16aa9869f9167798
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-11-02 05:08:27 +0000

    Add Date prop to BaseEntity

---
+++ Storage/Migrations/20221102033015_AddDateProp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Storage.Migrations
+{
+    public partial class AddDateProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "StateEntry",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "ActivityEntry",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "StateEntry");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "ActivityEntry");
+        }
+    }
+}
