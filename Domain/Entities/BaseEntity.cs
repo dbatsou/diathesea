ca38a1367bb8e185280c8f727668481933859dec
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-11-02 05:08:27 +0000

    Add Date prop to BaseEntity

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -4,6 +4,7 @@ namespace Domain.Entities
     {
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
+        public DateTime Date { get; set; }
         public string Note { get; set; }
     }
 }
