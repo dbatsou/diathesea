1721b67bc5d3b500c0d4ebdbc197ceeb7fa1b3b6
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-11-28 06:01:59 +0000

    Add routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,18 +1,20 @@
 import { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
-import StateEntriesDashboard from "../../features/StateEntries/StateEntriesDashboard";
 import LoadingComponent from "./LoadingComponent";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../stores/store";
+import { Route, Routes, useLocation } from "react-router-dom";
+import StateEntryInput from "../../features/StateEntries/StateEntryInput";
+import StateEntriesList from "../../features/StateEntries/StateEntriesList";
+import HomePage from "../../features/Homepage/HomePage";
 
 function App() {
   const { stateEntryStore, stateStore } = useStore();
-
+  const location = useLocation();
   useEffect(() => {
     stateStore.loadStates();
-    stateEntryStore.loadStateEntries();
-  }, [stateEntryStore, stateStore]);
+  }, [stateStore]);
 
   if (stateEntryStore.loadingInitial || stateStore.loadingInitial)
     return <LoadingComponent />;
@@ -20,7 +22,21 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: "4em" }}>
-        <StateEntriesDashboard />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            key={location.key}
+            path="/state/new"
+            element={<StateEntryInput />}
+          />
+          <Route
+            key={location.key}
+            path="/state/edit/:id"
+            element={<StateEntryInput />}
+          />
+          <Route path="/history" element={<StateEntriesList />} />
+        </Routes>
+        {/* <StateEntriesDashboard /> */}
       </Container>
     </>
   );
