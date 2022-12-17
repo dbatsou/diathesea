e792a1c6815f72194e461ba4024096e45290897b
Dimitrios Batsougiannis <jmpatsou@live.com>
2022-12-17 23:37:43 +0000

    switch url to go

--- APILoadTesting/Program.cs
+++ APILoadTesting/Program.cs
@@ -9,15 +9,16 @@ using NBomber.Plugins.Network.Ping;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 string baseUrl = "http://localhost:5000/api";
+string GObaseUrl = "http://localhost:8080";
 try
 {
-	CreateScenarios();
+    CreateScenarios();
 
 }
 catch (Exception e)
 {
-	Console.WriteLine(e);
-	throw;
+    Console.WriteLine(e);
+    throw;
 }
 Console.WriteLine("end of story");
 
@@ -26,54 +27,55 @@ Console.WriteLine("end of story");
 
 void CreateScenarios()
 {
-	var httpFactory = ClientFactory.Create(
-		name: "http_factory",                         
-		clientCount: 1,
-		initClient: (number,context) => Task.FromResult(new HttpClient())
-	);
-	var stateEntryBody = JsonSerializer.Serialize(new  {StateId = 1, Date = DateTime.Now, Note = "welcome to the jungle"});
-	
-	var steps = new[]
-	{
-		CreateStep(httpFactory,"getStates", "/state", HttpMethod.Get),
-		CreateStep(httpFactory,"getEntries", "/stateEntry", HttpMethod.Get),
-		CreateStep(httpFactory,"addStateEntry", "/stateEntry", HttpMethod.Post,stateEntryBody),
+    var httpFactory = ClientFactory.Create(
+        name: "http_factory",
+        clientCount: 5,
+        initClient: (number, context) => Task.FromResult(new HttpClient())
+    );
+    var stateEntryBody = JsonSerializer.Serialize(new { StateId = 1, Date = DateTime.Now, Note = "welcome to the jungle" });
+
+    var steps = new[]
+    {
+        CreateStep(httpFactory,"getStates", "/state", HttpMethod.Get),
+		//CreateStep(httpFactory,"getEntries", "/stateEntry", HttpMethod.Get),
+		//CreateStep(httpFactory,"addStateEntry", "/stateEntry", HttpMethod.Post,stateEntryBody),
 	};
-	
-	var scenario = ScenarioBuilder
-		.CreateScenario("addEntry", steps)
-		.WithWarmUpDuration(TimeSpan.FromSeconds(5))
-		.WithLoadSimulations(
-			Simulation.KeepConstant(10, during: TimeSpan.FromSeconds(30))
-		);
 
-	// creates ping plugin that brings additional reporting data
-	var pingPluginConfig = PingPluginConfig.CreateDefault(new[] { "localhost" });
-	var pingPlugin = new PingPlugin(pingPluginConfig);
+    var scenario = ScenarioBuilder
+        .CreateScenario("addEntry", steps)
+        .WithWarmUpDuration(TimeSpan.FromSeconds(0))
+        .WithLoadSimulations(
+            Simulation.KeepConstant(500, during: TimeSpan.FromSeconds(300))
+        );
 
-	NBomberRunner
-		.RegisterScenarios(scenario)
-		.WithWorkerPlugins(pingPlugin)
-		.WithReportFormats(ReportFormat.Html)
-		.Run();
+    // creates ping plugin that brings additional reporting data
+    var pingPluginConfig = PingPluginConfig.CreateDefault(new[] { "localhost" });
+    var pingPlugin = new PingPlugin(pingPluginConfig);
+
+    NBomberRunner
+        .RegisterScenarios(scenario)
+        .WithWorkerPlugins(pingPlugin)
+        .WithReportFormats(ReportFormat.Html)
+        .Run();
 }
 
-IStep CreateStep(IClientFactory<HttpClient> clientFactory, string name, string url, HttpMethod method, string body=null)
+IStep CreateStep(IClientFactory<HttpClient> clientFactory, string name, string url, HttpMethod method, string body = null)
 {
-	string uri = $"{baseUrl}{url}";
-	var step = Step.Create(name,
-		clientFactory: clientFactory,
-		execute: context =>
-		{
-			var request = Http.CreateRequest(method.Method, uri);
-			
-			if (!string.IsNullOrEmpty((body)))
-			{
-				var stringContent = new StringContent(body, Encoding.UTF8, "application/json");
-				request = request.WithBody(stringContent);
-			}
-			var resp=  Http.Send(request, context);
-			return resp;
-		});
-	return step;
+    baseUrl = GObaseUrl;
+    string uri = $"{baseUrl}{url}";
+    var step = Step.Create(name,
+        clientFactory: clientFactory,
+        execute: context =>
+        {
+            var request = Http.CreateRequest(method.Method, uri);
+
+            if (!string.IsNullOrEmpty((body)))
+            {
+                var stringContent = new StringContent(body, Encoding.UTF8, "application/json");
+                request = request.WithBody(stringContent);
+            }
+            var resp = Http.Send(request, context);
+            return resp;
+        });
+    return step;
 }
