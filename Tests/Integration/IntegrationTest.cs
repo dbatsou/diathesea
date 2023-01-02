2cc0763dddda9f22a67b5e91ab4cf3f3abe75a5c
Dimitrios Batsougiannis <jmpatsou@live.com>
2023-01-02 01:27:36 +0000

    Add Integrationtests, adjust project files

---
+++ Tests/Integration/IntegrationTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json.Linq;
+
+namespace Tests.Integration;
+
+public class IntegrationTest
+{
+    private HttpClient _httpClient;
+    public IntegrationTest()
+    {
+        var webAppFactory = new WebApplicationFactory<Program>();
+        _httpClient = webAppFactory.CreateDefaultClient();
+    }
+    
+    [Fact]
+    public async Task DefaultIntegrationTest()
+    {
+        var response = await _httpClient.GetAsync("");
+        var stringResult = await response.Content.ReadAsStringAsync();
+        
+        Assert.Equal("Hello World!",stringResult);
+    }
+    
+    [Fact]  
+    public async Task GetStates_Returns_108_States()
+    {
+        var response = await _httpClient.GetAsync("/api/State");
+        var stringResult = await response.Content.ReadAsStringAsync();
+        JArray result = JArray.Parse(stringResult);
+
+        Assert.True(response.IsSuccessStatusCode);
+        Assert.Equal(108,result.Count);
+    }
+}
