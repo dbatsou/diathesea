bb70bc011499622d31ea9ef135ebb53b61ad5309
Dimitrios Batsougiannis <jmpatsou@live.com>
2023-02-01 23:40:05 +0000

    Add Password Hash / Salt to User table

---
+++ Storage/Migrations/20230201171953_AddPasswordSalthash.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordSalthash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "User",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "User",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "User");
+        }
+    }
+}
