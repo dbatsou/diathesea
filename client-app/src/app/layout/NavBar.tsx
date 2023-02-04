86ed756d8688a7d3e2f9dda27cb3cc3185e24279
Dimitrios Batsougiannis <jmpatsou@live.com>
2023-02-04 04:27:44 +0000

    Add new Menu Items

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,18 +1,40 @@
 import { NavLink } from "react-router-dom";
-import { Container, Menu } from "semantic-ui-react";
+import { Container, Grid, Menu, Segment } from "semantic-ui-react";
+import { useStore } from "../stores/store";
+import Utils from "../helpers/visibilityHelper";
+import { observer } from "mobx-react-lite";
+export default observer(function NavBar() {
+  const { authStore } = useStore();
+  const { isLoggedIn } = authStore;
 
-export default function NavBar() {
   return (
     <Container>
       <Menu size="large" inverted fixed="top">
         <Menu.Item header as={NavLink} to="/">
           diathesea
         </Menu.Item>
-        <Menu.Item header as={NavLink} to="/state/new">
+        <Menu.Item
+          header
+          as={NavLink}
+          to="/state/new"
+          style={{ display: Utils.isVisible(isLoggedIn) }}
+        >
           New
         </Menu.Item>
-        <Menu.Item header as={NavLink} to="/history">
+        <Menu.Item
+          header
+          as={NavLink}
+          to="/history"
+          style={{ display: Utils.isVisible(isLoggedIn) }}
+        >
           History
+        </Menu.Item>
+        <Menu.Item
+          header
+          as={NavLink}
+          style={{ display: Utils.isVisible(isLoggedIn) }}
+        >
+          Logout
         </Menu.Item>
         {/* <Menu.Item name="States" /> */}
         {/* <Menu.Item name="History" /> */}
@@ -20,4 +42,4 @@ export default function NavBar() {
       </Menu>
     </Container>
   );
-}
+});
