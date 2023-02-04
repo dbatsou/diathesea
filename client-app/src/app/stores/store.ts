3b92adba806c5a7e337f46a0dc37d473221220ab
Dimitrios Batsougiannis <jmpatsou@live.com>
2023-02-04 04:27:36 +0000

    Add authStore

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -2,17 +2,20 @@
     Store that holds other stores
 */
 import { createContext, useContext } from "react";
+import AuthStore from "./authStore";
 import StateEntryStore from "./stateEntryStore";
 import StateStore from "./stateStore";
 
 interface Store {
   stateEntryStore: StateEntryStore;
   stateStore: StateStore;
+  authStore: AuthStore;
 }
 
 export const store: Store = {
   stateEntryStore: new StateEntryStore(),
   stateStore: new StateStore(),
+  authStore: new AuthStore(),
 };
 
 export const StoreContext = createContext(store);
