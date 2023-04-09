1daa057ab964828f51c15ddff1a652c714645660
Dimitrios Batsougiannis <jmpatsou@live.com>
2023-04-09 15:30:56 +0100

    Remove register component

--- client-app/src/features/Homepage/HomePage.tsx
+++ client-app/src/features/Homepage/HomePage.tsx
@@ -1,6 +1,5 @@
 import { Container } from "semantic-ui-react";
 import Login from "../Login/Login";
-import Register from "../Register/Register";
 
 export default function HomePage() {
   return (
@@ -8,7 +7,6 @@ export default function HomePage() {
       {/* <h1>Hello, is there anybody out there?</h1>
        */}
       <Login />
-      {/* <Register /> */}
     </Container>
   );
 }
